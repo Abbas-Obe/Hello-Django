e3ce0488eaf84f10096942fa2c9a7dd17a019b56
Abbas-Obe <obeabbas416@gmail.com>
2021-03-11 14:27:16 +0100

    Initial commit

--- pages/urls.py
+++ pages/urls.py
@@ -4,6 +4,5 @@ from .views import homePageView, home, ourPost
 
 urlpatterns=[
     path("", homePageView),
-    path("abbas", home),
-    path("abbas/second", ourPost)
+    
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,10 +4,5 @@ from django.http import HttpResponse
 def homePageView(request):
     return HttpResponse("Welcome to my blog")
 
-def home(request):
-    return HttpResponse("Welcome to the home page")
-
-def ourPost(request):
-    return HttpResponse("this is a post of abbas")
   
 # Create your views here.
